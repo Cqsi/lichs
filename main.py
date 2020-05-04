cc6c7a88bc2329e5cea8ba9a6efb1c91f9a39248
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-04 13:03:10 +0300

    somehow got something to work, don't know why

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ client = berserk.Client(session)
 # Gets your account data, e.g ["id"], ["username"]
 account_data = client.account.get()
 #print(account_data)
+
+is_polite = True
+for event in client.bots.stream_incoming_events():
+    print(event)
+    if event['type'] == 'challenge':
+        client.bots.accept_challenge(event['challenge']['id'])
+    elif event['type'] == 'gameStart':
+        game = Game(event['id'])
+        game.start()
