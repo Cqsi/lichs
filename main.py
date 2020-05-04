d70779ff039ac89ecda4612ce4c0c5cabcc770f5
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-04 13:41:16 +0300

    testing some game shit

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import berserk
 import chess
+import threading
+
+class Game(threading.Thread):
+    def __init__(self, client, game_id, **kwargs):
+        super().__init__(**kwargs)
+        self.game_id = game_id
+        self.client = client
+        self.stream = client.bots.stream_game_state(game_id)
+        self.current_state = next(self.stream)
+
+    def run(self):
+        for event in self.stream:
+            if event['type'] == 'gameState':
+                self.handle_state_change(event)
+            elif event['type'] == 'chatLine':
+                self.handle_chat_line(event)
+
+    def handle_state_change(self, game_state):
+        print("State changed")
+
+    def handle_chat_line(self, chat_line):
+        pass
 
 with open("C:\\Users\\Petter\\Desktop\\PythonProjects\\lichess_token.txt") as f:
     token = f.read()
@@ -13,9 +35,10 @@ account_data = client.account.get()
 
 is_polite = True
 for event in client.bots.stream_incoming_events():
-    print(event)
     if event['type'] == 'challenge':
         client.bots.accept_challenge(event['challenge']['id'])
     elif event['type'] == 'gameStart':
-        game = Game(event['id'])
+        print(event)
+        game = Game(client, event['game']['id'])
         game.start()
+
