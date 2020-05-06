2196055cea6f1bb1c5f96c21d8cb844c0e519884
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-06 16:56:08 +0300

    moved some files, got a understanding of gameState event, didn't change much

--- testing/Game.py
+++ testing/Game.py
@@ -2,59 +2,41 @@ import threading
 import chess
 
 chess_board = chess.Board()
-move_count = 0
-isWhite = True
-color = "Black"
+move_count = -1
 
 
 class Game(threading.Thread):
 
-    def __init__(self, board, game_id, player_id, **kwargs):
+    def __init__(self, board, game_id, **kwargs):
         super().__init__(**kwargs)
         self.game_id = game_id
         self.board = board
         self.stream = board.stream_game_state(game_id)
         self.current_state = next(self.stream)
-        self.player_id = player_id
 
     def run(self):
         for event in self.stream:
             if event['type'] == 'gameState':
+                print(event)
                 self.handle_state_change(event)
             elif event['type'] == 'chatLine':
                 self.handle_chat_line(event)
 
     def handle_state_change(self, game_state):
-        
-        # big spaghetti code alert, horrible code below needs improvement
-        # TODO: What if you're white?
-
+        # test code, this program is always black
         global move_count
         global chess_board
-        global isWhite
-        global color
 
-        if move_count == 0:
-            if game_state["white"]["id"] != self.player_id:
-                isWhite = False
-                color = "White"
-
+        move_count+=1
         if move_count%2==0:
-            print(color + " moved.")
-            print()
-            
+            print("White moved.")
             chess_board.push_uci(game_state["moves"].split()[-1])
             print(chess_board)
-            print()
 
             move = input("Make your move: ")
             self.board.make_move(self.game_id, move)
             chess_board.push_uci(move)
             print(chess_board)
-            print()
-            print(color + "'s turn...")
-        
-        move_count+=1
         
         # Another example is moving knight from g8 to f6
         # self.board.make_move(self.game_id, "g8f6")
--- testing/main.py
+++ testing/main.py
@@ -15,25 +15,16 @@ board = berserk.clients.Board(session)
 account_data = client.account.get()
 player_id = account_data["id"]
 
-# Welcome text
-print("Welcome to Lichess!\n")
-print("What kind of chess do you want to play?")
-print("1. Rapid (10+0)\n2. Classical (30+0)\n")
-num = input("Enter 1 or 2: ")
-time = 0
-
-if num=="1":
-    time=10
-elif num=="2":
-    time=30
-else:
-    # This needs improvement, something like a while/for loop
-    print("Something went wrong, please enter the lichess command again.")
-    sys.exit()
-
-board.seek(time, 0)
-
 for event in board.stream_incoming_events():
-    if event['type'] == 'gameStart':
-        game = Game.Game(board, event['game']['id'], player_id)
+    if event['type'] == 'challenge':
+        print("Challenge time!!!")
+
+        # Accepts the challenge, mainly used for testing
+        game_id = event['challenge']['id']
+        board.accept_challenge(game_id)
+
+        # Post message in chat
+        #board.post_message(game_id, "Hello noob!")
+    elif event['type'] == 'gameStart':
+        game = Game.Game(board, event['game']['id'])
         game.start()
