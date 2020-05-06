fe37f23b58df72b60328ec8c7ff44eee69b277dd
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-06 13:57:57 +0300

    added a testing folder

--- testing/main.py
+++ testing/main.py
@@ -15,24 +15,6 @@ board = berserk.clients.Board(session)
 account_data = client.account.get()
 player_id = account_data["id"]
 
-# Welcome text
-print("Welcome to Lichess!\n")
-print("What kind of chess do you want to play?")
-print("1. Rapid (10+0)\n2. Classical (30+0)")
-num = input("Enter 1 or 2: ")
-time = 0
-
-if num=="1":
-    time=10
-elif num=="2":
-    time=30
-else:
-    # This needs improvement, something like a while/for loop
-    print("Something went wrong, please enter the lichess command again.")
-    sys.exit()
-
-board.seek(time, 0)
-
 is_polite = True
 for event in board.stream_incoming_events():
     if event['type'] == 'challenge':
