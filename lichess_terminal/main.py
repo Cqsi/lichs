6dc5d48e083be1288965d3fdad3cc894836a0251
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-13 19:34:31 +0300

    edit

---
+++ lichess_terminal/main.py
@@ -0,0 +1,58 @@
+import sys
+import berserk
+import chess
+
+import Game
+
+def main():
+
+    with open("C:\\Users\\Petter\\Desktop\\PythonProjects\\lichess_token.txt") as f:
+        token = f.read()
+
+    session = berserk.TokenSession(token)
+    client = berserk.clients.Client(session)
+    board = berserk.clients.Board(session)
+
+    # Gets your account data, e.g ["id"], ["username"]
+    account_data = client.account.get()
+    player_id = account_data["id"]
+
+    # Welcome text
+    print("Welcome to Lichess!\n")
+    print("What kind of chess do you want to play?")
+    print("1. Rapid (10+0)\n2. Classical (30+0)\n")
+    num = input("Enter 1 or 2: ")
+    time = 0
+
+    if num=="1":
+        time=10
+    elif num=="2":
+        time=30
+    else:
+        # This needs improvement, something like a while/for loop
+        print("Something went wrong, please enter the lichess command again.")
+        sys.exit()
+
+    board.seek(time, 0)
+    print("Searching after opponent...")
+
+    for event in board.stream_incoming_events():
+        if event['type'] == 'gameStart':
+
+            print("An opponent was found!")
+
+            isWhite = True
+            color = "Black" # We set the color to the opposite color of the player
+            
+            if player_id != client.games.export(event['game']['id'])['players']['white']['user']['id']:
+                isWhite = False
+                color = "White"
+                print("You're playing as black!")
+                print("White's turn...")
+            else:
+                print("You're playing as white!")
+            game = Game.Game(board, event['game']['id'], player_id, isWhite, color)
+            game.start()
+        
+if __name__=="__main__":
+    main()
--- setup.py
+++ setup.py
@@ -11,12 +11,19 @@ setuptools.setup(
     description="Play chess against other real players in your terminal using Lichess",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/Cqsi/lichess_terminal",
+    packages=["lichess-terminal"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Windows",
     ],
     python_requires='>=3.6',
+    install_requires=["python-chess", "berserk"]
+    entry_points={
+        "console_scripts": [
+            "lichess=lichess_terminal.__main__:main",
+        ]
+    },
+    
 )
