12a1cadef3fb51b4ec405a1532a2f350786ef679
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-14 20:47:20 +0300

    edit

--- lichess_terminal/testing/Game.py
+++ lichess_terminal/testing/Game.py
@@ -57,8 +57,10 @@ class Game(threading.Thread):
                         move = input("Make your move: ")
                         if move.lower() == "resign":
                             self.board.resign_game(self.game_id)
-                        self.board.make_move(self.game_id, move)
-                        chess_board.push_uci(move)
+                            os._exit(0)
+                        else:
+                            self.board.make_move(self.game_id, move)
+                            chess_board.push_uci(move)
                     except:
                         print("You can't make that move. Try again!")
                         continue
--- lichess_terminal/testing/main.py
+++ lichess_terminal/testing/main.py
@@ -1,6 +1,7 @@
 import sys
 import berserk
 import chess
+import os
 
 import Game
 
