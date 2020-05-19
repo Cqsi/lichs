ebc80e4315447c8bf943652f3ad996aa75ef2e30
Casimir Rönnlöf <casimirr04@gmail.com>
2020-05-19 14:55:23 +0300

    delete

--- build/lib/lichs/api_key.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pathlib
-
-def set_api(key):
-    
-    file = open(str(pathlib.Path(__file__).parent.absolute()) + "\\key.txt", "w")
-    file.write(key)
-    file.close()
-    print("The API-key " + key + " was entered and saved.")
