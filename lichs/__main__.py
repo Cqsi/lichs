646ca6c5ce1e509f0c40f1e03c327b096972fe99
Renato Lacerda <renato.ac.lacerda@gmail.com>
2020-05-22 16:51:44 -0300

    Changed function to write and load token key
    Instead of trying to write the key and raise an exception, it will instead:
    
    - Check if a key was passed as argument, if so write the key.
    
    - If there was no argument, check if the key file exists.
    
    - If there is no key file, ask the user for a token, and write the token
    into the key file.

--- lichs/__main__.py
+++ lichs/__main__.py
@@ -10,59 +10,74 @@ from pathlib import Path
 from lichs.Game import Game
 from lichs.api_key import set_api
 
+token_file = Path(__file__).parent.absolute() / "key"
+
+
+def set_token(key):
+    token_file.write_text(key)
+    print("The API-token " + key + " was entered and saved.")
+
+
+
 def main():
+    if len(sys.argv) == 2:
+            set_token(sys.argv[1])
+
+    if not token_file.exists():
+        print("Please provid a token key")
+        print("See the instructions in the Github README:")
+        print("https://github.com/Cqsi/lichs#how-to-generate-a-personal-api-token")
+        key = input("token: ")
+        set_token(key)
 
     try:
-        set_api(sys.argv[1])
-        os._exit(0)
+        token = token_file.read_text()
+        session = berserk.TokenSession(token)
+        client = berserk.clients.Client(session)
+        board = berserk.clients.Board(session)
     except:
-        try:
-            token_file = Path(__file__).parent.absolute() / "key"
-            token = token_file.read_text()
-            session = berserk.TokenSession(token)
-            client = berserk.clients.Client(session)
-            board = berserk.clients.Board(session)
-        except:
-            print("The API token is either empty or wrong. Please run the command 'lichess' and input your API token as a second argument, i.e 'lichs <api_token>'. If you need more help, please see the instructions in the Github README: \nhttps://github.com/Cqsi/lichs#how-to-generate-a-personal-api-token")
-            os._exit(0)
+        print("Unable to connect Lichess")
+        print("Check if your token key is right")
+        print("Or try again later")
+        os._exit(0)
 
-        # Gets your account data, e.g ["id"], ["username"]
-        account_data = client.account.get()
-        player_id = account_data["id"]
+    # Gets your account data, e.g ["id"], ["username"]
+    account_data = client.account.get()
+    player_id = account_data["id"]
 
-        # Welcome text
-        print("Welcome to Lichess!\n")
-        print("What kind of chess do you want to play?")
-        print("1. Rapid (10+0)\n2. Classical (30+0)\n")
-        num = input("Enter 1 or 2: ")
-        time = 0
+    # Welcome text
+    print("Welcome to Lichess!\n")
+    print("What kind of chess do you want to play?")
+    print("1. Rapid (10+0)\n2. Classical (30+0)\n")
+    num = input("Enter 1 or 2: ")
+    time = 0
 
-        if num=="1":
-            time=10
-        elif num=="2":
-            time=30
-        else:
-            # This needs improvement, something like a while/for loop
-            print("Something went wrong, please enter the lichess command again.")
-            sys.exit()
+    if num=="1":
+        time=10
+    elif num=="2":
+        time=30
+    else:
+        # This needs improvement, something like a while/for loop
+        print("Something went wrong, please enter the lichess command again.")
+        sys.exit()
 
-        print("Searching after opponent...")
-        board.seek(time, 0)
+    print("Searching after opponent...")
+    board.seek(time, 0)
 
-        for event in board.stream_incoming_events():
-            if event['type'] == 'gameStart':
+    for event in board.stream_incoming_events():
+        if event['type'] == 'gameStart':
 
-                print("An opponent was found!")
+            print("An opponent was found!")
 
-                isWhite = True
-                color = "Black" # We set the color to the opposite color of the player
+            isWhite = True
+            color = "Black" # We set the color to the opposite color of the player
                 
-                if player_id != client.games.export(event['game']['id'])['players']['white']['user']['id']:
-                    isWhite = False
-                    color = "White"
-                    print("You're playing as black!")
-                    print("White's turn...")
-                else:
-                    print("You're playing as white!")
-                game = Game(board, event['game']['id'], player_id, isWhite, color)
-                game.start()
+            if player_id != client.games.export(event['game']['id'])['players']['white']['user']['id']:
+                isWhite = False
+                color = "White"
+                print("You're playing as black!")
+                print("White's turn...")
+            else:
+                print("You're playing as white!")
+            game = Game(board, event['game']['id'], player_id, isWhite, color)
+            game.start()
